06f7d079e38d6df52d9a1ea6a098524a95fd496e
GiantsNYFan <drewpridemore+github@gmail.com>
2025-03-18 22:03:41 +0800

    moonraker.conf modified - 03/18/2025 - 10:03:31 PM

--- Desktop/myfile/znp/znp_tjc_klipper/build/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Start makerbase-client"
-time=$(date "+%Y%m%d%H%M%S")
-# /root/makerbase-client/build/MakerbaseClient localhost > /root/mksclient/test-$time.log
-/home/mks/Desktop/myfile/znp/znp_tjc_klipper/build/io -4 0xff100028 0x010000
-/home/mks/Desktop/myfile/znp/znp_tjc_klipper/build/znp_tjc_klipper localhost >/dev/null 2>&1
